715d62a44f589a5fcec3ee66e98f234fcdef2d57
dl447 <dereklee9ab@aol.com>
2017-04-21 10:32:14 -0400

    rnfugyerguehrknvjrw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,54 +4,54 @@ import java.io.File;
 import java.util.*;
 
 public class Main {
-    public static ArrayList<Student>  list = new ArrayList<>();
-    public static void main(String[] args) throws java.io.IOException{
+    public static ArrayList<Student> list = new ArrayList<>();
+
+    public static void main(String[] args) throws java.io.IOException {
         Scanner sc = new Scanner(new File("data.txt"));
         sc.nextLine();
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             try {
                 int num = sc.nextInt();
 
                 if (getStudent(num) == null) {
                     list.add(new Student(num));
                 }
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 sc.nextLine();
             }
         }
         Scanner sc2 = new Scanner(new File("data.txt"));
         sc2.nextLine();
-        while(sc2.hasNextLine()){
+        while (sc2.hasNextLine()) {
             String line = sc2.nextLine();
             Scanner sc3 = new Scanner(line);
             int id = sc3.nextInt();
             sc3.useDelimiter("\\t");
             double frq1 = 0;
             double frq2 = 0;
-            while(sc3.hasNext()){
+            while (sc3.hasNext()) {
                 int fr = 1;
                 String portion = sc3.next();
-                if(portion.contains("\"")){
+                System.out.println(portion);
+                if (portion.matches(".*\\d+.*")) {
+                    portion = RNNTC(portion);
                     portion = portion.replaceAll("[^0123456789.\\s]", "");
+                    System.out.println(portion);
                     Scanner sc4 = new Scanner(portion);
-                    while(sc4.hasNextDouble()){
-                        if(fr == 1) {
+                    while (sc4.hasNextDouble()) {
+                        if (fr == 1) {
                             frq1 += sc4.nextDouble();
-                        }
-                        else{
+                        } else {
                             frq2 += sc4.nextDouble();
                         }
                     }
-                }
-                else if(!portion.isEmpty()){
+                } else if (!portion.isEmpty()) {
 
-                    if((portion.replaceAll("[0123456789]", "").isEmpty())){
-                        if(fr == 1){
+                    if ((portion.replaceAll("[0123456789]", "").isEmpty())) {
+                        if (fr == 1) {
                             frq1 -= (Integer.parseInt(portion)) * .25;
                             fr++;
-                        }
-                        else{
+                        } else {
 
                             frq2 -= (Integer.parseInt(portion)) * .25;
                             break;
@@ -61,37 +61,56 @@ public class Main {
             }
             int l = 0;
             int i = 0;
-            while(i < list.size()){
-                if(list.get(i).secret == id){
+            while (i < list.size()) {
+                if (list.get(i).secret == id) {
                     l = i;
                 }
                 i++;
             }
-            list.get(l).r1(frq1);
-            list.get(l).r2(frq2);
+            Student x = list.get(l);
+            x.r1(frq1);
+            x.r2(frq2);
+            list.set(l, x);
         }
-        for (int i = 0; i < list.size(); i++) {
+        /*for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i).frq1);
             System.out.println(list.get(i).frq2);
-        }
+        }*/
 
 
     }
-    public static Student getStudent(int n){
+
+    public static Student getStudent(int n) {
         int i = 0;
-        while(i < list.size()){
-            if(list.get(i).secret == n){
+        while (i < list.size()) {
+            if (list.get(i).secret == n) {
                 return list.get(i);
             }
             i++;
         }
         return null;
     }
-    public static void setNames() throws java.io.IOException{
+
+    public static void setNames() throws java.io.IOException {
         Scanner sc = new Scanner(new File("names.txt"));
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             int num = sc.nextInt();
             getStudent(num).setName(sc.next());
         }
     }
+
+    public static String RNNTC(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.substring(i, i + 1).replaceAll("[a-zA-Z]", "").isEmpty()) {
+                try {
+                    s = s.substring(0, i) + s.substring(i + 2, s.length());
+                }
+                catch(java.lang.StringIndexOutOfBoundsException e){
+                }
+            }
+        }
+
+        return s;
+    }
 }
+
